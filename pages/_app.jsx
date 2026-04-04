7e4fe9f5b06d1ceb6a7ed8ab9909d7fe289f6628
Horacio Soria <horaciosu@MacBook-Pro-de-Horacio.local>
2026-04-03 23:59:20 -0700

    fix: default language spanish

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import '../styles/globals.css'
 
 export default function App({ Component, pageProps }) {
-  const [lang, setLang] = useState('en')
+  const [lang, setLang] = useState('es')
 
   return <Component {...pageProps} lang={lang} setLang={setLang} />
 }
